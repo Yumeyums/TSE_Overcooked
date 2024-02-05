c9c9a35d8713e10b84ac440b3902ac6fe5c7b409
Ellie Ringwood <123974599+Ellie-Ringwood@users.noreply.github.com>
2024-02-05 16:05:26 +0000

    Pasta and Rice added :)

--- Scripts/itemScript.cs
+++ Scripts/itemScript.cs
@@ -3,6 +3,18 @@ using System;
 
 public partial class itemScript : RigidBody3D
 {
+	[Export]
+	public bool canCook;
+	[Export]
+	public bool canBoil;
+	[Export]
+	public bool canChop;
+	[Export]
+	public bool canClean;
+	[Export]
+	public bool canSubmit;
+	[Export]
+	public bool canPlate;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
