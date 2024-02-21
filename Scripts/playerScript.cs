607381ebe783eb4d7bda522fd8443d7bd87157d5
MiloB04 <127067268+MiloB04@users.noreply.github.com>
2024-02-21 14:32:15 +0000

    Rough Implementation of movement
    Implementation of acceleration

--- Scripts/playerScript.cs
+++ Scripts/playerScript.cs
@@ -4,10 +4,14 @@ using System;
 public partial class playerScript : CharacterBody3D
 {
 	[Export]
-	public int Speed { get; set; } = 14;
+	public float Speed { get; set; } = 0;
 	[Export]
 	public int playerNumber;     // distinguishes between players
-
+	public float mspeed = 20.0f;			//Maximum speed the player can reach.
+	public float accelleration = 1.25f;	//The amount of speed the player gains every second by moving.
+	public float friction = 0.60f; 		//The amount of speed the player loses every second.
+	
+	
 	private Vector3 _targetVelocity = Vector3.Zero;
 
 	public override void _PhysicsProcess(double delta)
@@ -16,19 +20,25 @@ public partial class playerScript : CharacterBody3D
 		if (playerNumber==1){
 			if (Input.IsActionPressed("move_right"))
 			{
-				direction.X += 1.0f;
+				direction.X += 1.0f; //Sets the X axis direction to positive
+				//Formula for speed calculation:
+				
+				
 			}
 			if (Input.IsActionPressed("move_left"))
 			{
-				direction.X -= 1.0f;
+				direction.X -= 1.0f; //Sets the X axis direction to negative
+				
 			}
 			if (Input.IsActionPressed("move_back"))
 			{
-				direction.Z += 1.0f;
+				direction.Z += 1.0f; //Sets the Z axis direction to positive
+				
 			}
 			if (Input.IsActionPressed("move_forward"))
 			{
-				direction.Z -= 1.0f;
+				direction.Z -= 1.0f; //Sets the Z axis direction to negative
+				
 			}
 			if (Input.IsActionPressed("interact"))
 			{
@@ -37,23 +47,46 @@ public partial class playerScript : CharacterBody3D
 					GD.Print("interact 1");   // start of code to interact with objects using Area3D, not done yet
 				//}
 			}
+			
+			if (!Input.IsActionPressed("move_forward") 	&&
+				!Input.IsActionPressed("move_back")		&&
+				!Input.IsActionPressed("move_left")		&&
+				!Input.IsActionPressed("move_right")	)
+				{	//If no movement buttons are being pressed
+					if((Speed-friction)<=0){	//Speed depletes twice as fast.
+						Speed = 0;
+					} else {
+						Speed -= friction;
+					}
+				} else { //Otherwise we accellerate
+					// First we check that the current speed + the acceleration doesn't exceed the max speed.
+					// if it doesn't we increase the speed as normal via acceleration.
+					// otherwise; we set the speed to the maximum speed.
+					if((Speed+accelleration)<mspeed){ Speed += accelleration; } else { Speed = mspeed; }
+				}
+				
+			
 		}
 		else if (playerNumber==2){
 			if (Input.IsActionPressed("move_right2"))
 			{
 				direction.X += 1.0f;
+				
 			}
 			if (Input.IsActionPressed("move_left2"))
 			{
 				direction.X -= 1.0f;
+				
 			}
 			if (Input.IsActionPressed("move_back2"))
 			{
 				direction.Z += 1.0f;
+				
 			}
 			if (Input.IsActionPressed("move_forward2"))
 			{
 				direction.Z -= 1.0f;
+				
 			}
 			if (Input.IsActionPressed("interact2"))
 			{
@@ -62,6 +95,28 @@ public partial class playerScript : CharacterBody3D
 					GD.Print("interact 2");
 				//}
 			}
+			if (!Input.IsActionPressed("move_forward2") 	&&
+				!Input.IsActionPressed("move_back2")		&&
+				!Input.IsActionPressed("move_left2")		&&
+				!Input.IsActionPressed("move_right2")	)
+				{	//If no movement buttons are being pressed
+					if((Speed-friction)<=0){	//Speed depletes twice as fast.
+						Speed = 0;
+					} else {
+						Speed -= friction;
+					}
+				} else { //Otherwise we accellerate
+					// First we check that the current speed + the acceleration doesn't exceed the max speed.
+					// if it doesn't we increase the speed as normal via acceleration.
+					// otherwise; we set the speed to the maximum speed.
+					if((Speed+accelleration)<mspeed){ Speed += accelleration; } else { Speed = mspeed; }
+				}
+		}
+
+		if((Speed-friction)<=0){
+			Speed = 0;
+		} else {
+			Speed -= friction;
 		}
 
 		if (direction != Vector3.Zero)
