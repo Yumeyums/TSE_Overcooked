f0d96b1610392bb282ee64c6a8de32fa7d313c1e
billyvick <25815409@students.lincoln.ac.uk>
2024-03-04 02:25:47 +0000

    timerworking

---
+++ Scenes/Timer.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+public partial class TimerScript : Node
+{
+	private int seconds = 0;
+	private int defaultSeconds = 60;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ResetTimer();
+		GetNode<Timer>("TimerNodeName").Start();
+	}
+	
+	// Called when the timer signals a timeout.
+private void OnTimerTimeout()
+{
+	GetNode<Label>("TimeRemaining").Text = seconds.ToString();
+	if (seconds > 0)
+	{
+		seconds--;
+	}
+}
+
+	// Function to reset the timer.
+	private void ResetTimer()
+	{
+		seconds = defaultSeconds;
+
+	}
+}
